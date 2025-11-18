29d083643ea0d9a65bbc8b6404c54a770863ffec
Jose Martos <pepemartos98@gmail.com>
2025-11-18 17:56:33 +0100

    nueva entrega

--- js/script.js
+++ js/script.js
@@ -6,36 +6,38 @@
 
 const url = `https://jsonplaceholder.typicode.com/users`;
 
-fetch(url)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("ERROR en la conexion con la url");
-    } else {
-      return response.json();
-    }
-  })
-  .then((datos) => {
-    let template = "";
-    
-    const nuevosUsuarios = datos.map((dato, i) => {
-        const {age, img, address, ...resto} = dato;
-        const {street, suite, city } = address;
+function getUsers() {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `Error a la hora de realizar la consulta. Error: ${response.status}`
+        );
+      } else {
+        return response.json();
+      }
+    })
+    .then((datos) => {
+      let template = "";
+
+      const nuevosUsuarios = datos.map((dato, i) => {
+        const { age, img, address, ...resto } = dato;
+        const { street, suite, city } = address;
 
         return {
-            ...resto,
-            address: {
-                usuario_address_street: `${street}`,
-                usuario_addres_suite: `${suite}`, 
-                usuario_addres_city: `${city}`
-            },
-            age: Math.floor(Math.random() * (55 - 18 + 1)) + 18,
-            img: `assets/img/${i + 1}.jpeg`
-        }
-    })
-    console.log(nuevosUsuarios);
-    nuevosUsuarios.forEach((elemento, i) => {
+          ...resto,
+          address: {
+            usuario_address_street: `${street}`,
+            usuario_addres_suite: `${suite}`,
+            usuario_addres_city: `${city}`,
+          },
+          age: Math.floor(Math.random() * (55 - 18 + 1)) + 18,
+          img: `assets/img/${i + 1}.jpeg`,
+        };
+      });
 
-      template += `
+      nuevosUsuarios.forEach((elemento, i) => {
+        template += `
         <li>
         <img src="${elemento.img}" alt="Imagen" />
          <h2><strong>Nombre:</strong> ${elemento.name}</h2>
@@ -47,7 +49,11 @@ fetch(url)
             <p><strong>Direccion:</strong> ${elemento.address.usuario_address_street}, ${elemento.address.usuario_addres_suite}, ${elemento.address.usuario_addres_city}</p>
         </li>
         `;
-    });
+      });
 
-    document.getElementById("listaUsuarios").innerHTML = template;
-  });
+      document.getElementById("listaUsuarios").innerHTML = template;
+    })
+    .catch((err) => console.log(err));
+}
+
+getUsers();
